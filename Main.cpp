49b5f7ec280998426e6b9562a596980731495604
Jose <jozho.g@gmail.com>
2014-10-17 12:20:54 -0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Cabecera.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    char op;
+    Lista Colegio [26];
+    cout << "Inicio Programa" << endl;
+    cout<<"Bienvenido a nuestro colegio!!"<<endl;
+    op=Menu();
+    switch (op){
+        case 'a':Inicializar(Colegio);break;
+        case 'b':;
+        case 'c':;
+        case 'd':;
+        case 'e':;
+        case 'f':;
+        case 'g':;
+        case 'h':;
+        case 'i':;
+        case 'j':;
+        case 'k':;
+    }
+    return 0;
+}
